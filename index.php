c5f648f341ef1c4d57670a4215f68899efceeb63
Gabriel P. Silva <gabriel.silva@gmail.com>
2023-06-01 12:18:04 -0300

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en,pt">
+<head>
+<meta name="google-site-verification" content="eiFedUayY431FC5elLMcriNuz-IHVbx4rKJt_wuB54E" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<meta name="description" content="Gabriel P. Silva"/> 
+<meta name="keywords" content="computador, programação paralela, arquitetura de computadores, sistemas operacionais, threads, computer, computer architecture, multithreading, processor, parallel, operating systems, unix, linux, accessibility, parallel programing, MPI, PVM, OpenMP, VHDL, Verilog, integrated circuits, digital, circuits, orca"/>
+<meta name="author" content="Gabriel P. Silva" /> 
+<link rel="stylesheet" type="text/css" href="default.css" media="screen"/>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-748MLZRBK4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-748MLZRBK4');
+</script>
+<title>Gabriel P. Silva</title>
+</head> <body>
+<!--div class="container"><div class="main"><div class="header">
+<div class="title"><h1><img style="width: 100px; height: 119px;" alt="Minerva" src="minerva.gif"/>&nbsp; &nbsp; &nbsp; -->
+<div class="container"><div class="main">
+<h1><img alt="Minerva" src="header4.jpg"/> <!--&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Gabriel P. Silva --> </h1>
+
+<?php include("menu.htm"); ?>
+
+<div class="content"><div class="item">
+<h2><a href="https://www.dcc.ufrj.br/">Institute of Computing</a>&nbsp;</h2>
+<h2><a href="https://www.ufrj.br/">Federal University of Rio de Janeiro</a></h2>
+<p> </p><div class="imagem"><img style="padding: 1px 10px; float: right; height: 119px; width: 100px;" src="gabriel2008.jpg" alt="Gabriel"><font size="2">Hello
+and welcome to my homepage.&nbsp;This site is intended to
+provide some information about myself and my work.&nbsp;I wrote
+some pages in English, but most of this site is indeed in Portuguese,
+due mainly to my undergraduate students. Please fell free to
+contact me either by&nbsp;e-mail or by phone, that you
+can find in the <a href="contact.php">contact</a>
+page.&nbsp;</font></div>
+<div style="text-align: justify;" class="descr"><br></div>
+</div>
+<div class="item"><h1><big>Classes</big></h1>
+<div class="descr"><p><font size="2">I am teaching or have teached the following classes at UFRJ:</font></p>
+<p><font size="2">Graduate:</font></p>
+<ul> 
+<li> <font size="2"><a href="https://www.dcc.ufrj.br/~gabriel/microarq/index.php">High Performance Microarchitecture - MAB 710</a></font></li>
+<li><font size="2"><a href="https://www.dcc.ufrj.br/~gabriel/sispar/index.php">Parallel Processing Systems Architecture - MAB 712</a></font></li>
+</ul>
+<p><font size="2"><br>Undergraduate:</font></p>
+<ul> 
+<li><font size="2"><a href="https://www.dcc.ufrj.br/~gabriel/progpar/index.php">Parallel and Distributed Programming - MAB 622&nbsp;</a> </font></li> 
+<li><font size="2"><a href="https://www.dcc.ufrj.br/~gabriel/arqcomp2/index.php">Computer Architeture II - MAB 369&nbsp;</a></font></li> 
+<li><font size="2"><a href="https://www.dcc.ufrj.br/%7Egabriel/circlog/index.php">Logic Circuits&nbsp; - MAB245</a></font></li>
+<li><font size="2"><a href="https://www.dcc.ufrj.br/%7Egabriel/arqcomp/index.php">Computer Architecture I&nbsp; - MAB355</a></font></li>
+<!--li><font size="2"><a href="https://www.dcc.ufrj.br/%7Egabriel/robotica/index.php">Introduction to Robotics &nbsp; - MABXXX </a></font></li-->
+</ul>
+</div>
+</div>
+<div class="item"><h1><big>Books</big></h1>
+<div class="descr"><p><font size="2">Some books I have published:</font></p>
+<ul> 
+<li><font size="2"><a href="https://www.casadocodigo.com.br/products/livro-programacao-paralela">Programação Paralela e Distribuída com MPI, OpenMP e OpenACC</a></font></li>
+<li> <font size="2"><a href="https://www.amazon.com.br/Programa%C3%A7%C3%A3o-Paralela-com-MPI-Introdut%C3%B3rio-ebook/dp/B079WNMF52">Programação Paralela com MPI: Um Curso Introdutório</a></font></li>
+<li><font size="2"><a href="https://www.amazon.com.br/SimuS-Simulador-Did%C3%A1tico-Arquitetura-Computadores-ebook/dp/B077MVC1P8/">SimuS: Um Simulador Didático para Arquitetura de Computadores</a></font></li>
+<li><font size="2"><a href="https://www.amazon.com.br/Simulador-Neander-X-Ensino-Arquitetura-Computadores-ebook/dp/B01DX2VLJA">O Simulador Neander-X: Para o Ensino de Arquitetura de Computadores</a></font></li>
+</ul>
+</div>
+</div>
+<div class="item"><h1><big>Useful Information</big></h1><font size="2">I have&nbsp;collected some useful information that you may check bellow:</font><div class="descr"><p></p>
+<ul>
+<li><font size="2"><a target="_blank" href="https://fedoralinux.tips">Fedora Linux Tips</a></font></li>
+<li><font size="2"><a target="_blank" href="https://iotjs.blogspot.com">Iot Blog</a></font></li>
+<!--li><font size="2"><a target="_blank" href="https://www.acm.uiuc.edu/webmonkeys/book/c_guide/">C Language Reference</a></font></li-->
+<li><font size="2"><a target="_blank" href="https://www.emerson.emory.edu/services/latex/latex_toc.html">Latex Reference Manual</a></font></li>
+<li><font size="2"> <a href="links.php">VHDL and FPGA</a></font></li>
+</ul></div></div>
+
+<div class="item"><h1><big>A Bit of History</big></h1><font size="2">Some old stuff I have&nbsp;collected:</font><div class="descr"><p></p>
+<ul>
+<li><font size="2"> <a href="x86.pdf">Intel 8086/88</a>- Julio Salek Aude</font></li>
+<li><font size="2"> <a href="ncesparc.pdf">NCE SPARC+</a>- Julio Salek Aude et alii</font></li>
+<li><font size="2"> <a href="Multiplus.pdf">Multiplus</a>- Julio Salek Aude et alii</font></li>
+
+<li><font size="2"><a href="https://www.dcc.ufrj.br/~gabriel/unixes.php"> The History of the Brazilian Unix-like Operational Systems (Portuguese)</a>- Newton Faller</font></li>
+<li><font size="2"><a href="https://www.dcc.ufrj.br/~gabriel/boletim/index.php"> Boletim do Plurix - Full Collection</a></font></li>
+<!--li><font size="2"><a href="https://techpack.acm.org/parallel"> Parallel Computing Tech Pack </a></font></li-->
+</ul></div></div>
+<div class="item"><h1><big>Calendar</big></h1>
+<div class="descr"><p><font size="2">My schedule:</font></p>
+<p><a target="_blank" href="https://www.google.com/calendar/render?cid=ttj9u83dsv6h1fjh2hgtpmcf4s%40group.calendar.google.com"><img src="https://www.google.com/calendar/images/ext/gc_button1_pt-BR.gif" border="0"></a></p>
+<ul>
+<!--li><font size="2"><a target="_blank" <a target="_blank" href="https://www.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=hr6ghaljhdfkbgbj6col00jmo8%40group.calendar.google.com&amp;color=%237A367A&amp;ctz=America%2FSao_Paulo" style=" border-width:0 " width="800" height="600" frameborder="0" scrolling="no">Academic Calendar at UFRJ - 2015</a></font></li-->
+</ul>
+<iframe src="https://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.dcc.ufrj.br%2F%7Egabriel&amp;layout=button_count&amp;show_faces=true&amp;width=450&amp;action=like&amp;font=trebuchet+ms&amp;colorscheme=light&amp;height=21" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>
+</div></div>
+
+<p><font size="2">Last &nbsp;Updated: 30.Jun.2022</font></p>
+</div></div>
+<div class="clearer"><span></span></div>
+<?php include("footer.htm"); ?>
+</body>
+</html>
+
