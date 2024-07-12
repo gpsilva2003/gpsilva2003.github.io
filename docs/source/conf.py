2356bf3a0aeac851c2a1278bfaa9b1af1d77f744
Gabriel P. Silva <gabriel.silva@gmail.com>
2024-07-12 10:08:42 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SimuS'
+copyright = '2024, Gabriel P. Silva'
+author = 'Gabriel'
 
 release = '0.1'
 version = '0.1.0'
